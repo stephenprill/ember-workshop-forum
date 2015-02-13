74c7ae803063eb1c5f775d7c6cdaa40f7db87320
Luke Bartel <bartelluke@gmail.com>
2015-02-12 20:21:29 -0700

    initial commit, added forums resource

---
+++ app/models/forum.js
@@ -0,0 +1,27 @@
+import DS from 'ember-data';
+
+var Forum = DS.Model.extend({
+  name: DS.attr('string'),
+  info: DS.attr('string'),
+  topics: DS.hasMany('topic', {async: true} )
+});
+
+Forum.reopenClass({
+  FIXTURES: [
+  {
+    id: 1,
+    name: 'Jazz between WW1 and WW2',
+    info: 'Talk about Jazz in the 20s and 30s',
+    topics: [1,4]
+  },
+  {
+    id: 2,
+    name: 'When Harry Met Sally',
+    info: 'Talk about best movie ever made, No haters',
+    topics: [1,4]
+  }
+  ]
+})
+
+
+});
--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource("forums", function() {
+    this.resource("topic", {path: '/:forum_id'});
+  });
 });
 
 export default Router;
